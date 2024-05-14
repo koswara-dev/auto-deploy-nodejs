27ffa89c557405c7b17455fca68e56b7f4fda170
Koswara <koswara.c79@gmail.com>
2024-05-14 09:31:01 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Import required modules
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Create an Express application
+const app = express();
+
+// Define routes
+app.get('/', (req, res) => {
+    res.send('GitHub Actions with Node.js, webservice is running successfully!');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
