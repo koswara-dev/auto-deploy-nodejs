84d223244f84d88e70e22458ea19c22b4095e25d
Koswara <koswara.c79@gmail.com>
2024-05-21 17:23:03 +0700

    update testing feature get data

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,7 +4,7 @@ const app = require('../app');
 // FILEPATH: /d:/workspace/react-workspace/backend/auto-deploy-nodejs/tests/app.test.js
 
 describe('GET /', () => {
-    it('should return status code 200 and "GitHub Actions with Node.js, webservice is running successfully!"', async () => {
+    it('should return status code 200 and "GitHub Actions with Node.js, webservice is running successfully! JuaraCoding"', async () => {
         const response = await request(app).get('/');
         expect(response.status).toBe(200);
         expect(response.text).toBe('GitHub Actions with Node.js, webservice is running successfully!');
